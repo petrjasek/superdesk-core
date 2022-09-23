9c7ba2907fe257a17df7e2162709056a7bc9716a
Petr Jasek <jasekpetr@gmail.com>
2022-09-23 17:11:21 +0200

    save

--- apps/rundowns/rundowns.py
+++ apps/rundowns/rundowns.py
@@ -1,9 +1,11 @@
 import datetime
 import superdesk
 
+from flask import json
 from typing import Optional
 
 from superdesk.metadata.item import metadata_schema
+from werkzeug.datastructures import ImmutableMultiDict
 
 from . import privileges, rundown_items, types, templates, shows
 
@@ -55,6 +57,22 @@ class RundownsResource(superdesk.Resource):
                     "value": superdesk.Resource.field("string", analyzed=True),
                 },
             },
+            "mapping": {
+                "type": "nested",
+                "properties": {
+                    "_id": {"type": "keyword"},
+                    "key": {"type": "keyword"},
+                    "value": metadata_schema["body_html"]["mapping"],
+                },
+            },
+        },
+        "rundown": {
+            "type": "dict",
+            "readonly": True,
+        },
+        "rundown_items": {
+            "type": "list",
+            "readonly": True,
         },
     }
 
@@ -184,6 +202,79 @@ class RundownsService(superdesk.Service):
             rundown_items.items_service.sync_items(updates, rundown["items"])
             if updates:
                 self.system_update(rundown["_id"], updates, rundown)
+
+    def get(self, req, lookup):
+        if req  and req.args.get("q"):
+            query = {
+                "nested": {
+                    "path": "items_data",
+                    "query": {
+                        "bool": {
+                            "must": [
+                                {
+                                    "query_string": {
+                                        "query": req.args.get("q"),
+                                        "default_operator": "AND",
+                                        "lenient": True,
+                                    },
+                                },
+                            ],
+                        },
+                    },
+                    "inner_hits": {
+                        "name": "items",
+                    },
+                },
+            }
+
+            source = json.loads(req.args.get("source")) if req.args.get("source") else {}
+            if source.get("query"):
+                old_query = source["query"]
+                source["query"] = {"bool": {
+                    "should": [
+                        old_query,
+                        query,
+                    ],
+                    "minimum_should_match": 1,
+                }}
+            else:
+                source["query"] = query
+            
+            print("source", json.dumps(source, indent=2))
+
+            args = req.args.to_dict()
+            args.pop("q")
+            args["source"] = json.dumps(source)
+            req.args = ImmutableMultiDict(args)
+            cursor = super().get(req, lookup)
+            matching_items = set()
+            for item in cursor.hits["hits"]["hits"]:
+                if item.get("inner_hits"):
+                    ids = [
+                        hit["_source"]["_id"]
+                        for hit in item["inner_hits"]["items"]["hits"]["hits"]
+                    ]
+                    for id in ids:
+                        matching_items.add(id)
+            if matching_items:
+                items = list(rundown_items.items_service.get_from_mongo(req=None, lookup={"_id": {"$in": list(matching_items)}}))
+                for rundown in cursor:
+                    if not rundown.get("items"):
+                        continue
+                    inner_items = []
+                    for ref in rundown["items"]:
+                        for item in items:
+                            if str(item["_id"]) == str(ref["_id"]):
+                                inner_items.append(item)
+                    rundown["rundown_items"] = inner_items
+            return cursor
+
+            for doc in cursor:
+                rundown = doc.copy()
+                doc.clear()
+                doc["rundown"] = rundown
+            return cursor
+        return super().get(req, lookup)
 
 
 rundowns_service = RundownsService()
