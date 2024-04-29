b4ba3cabd4a784e98b98d3d3505a39a69427726c
Petr Jasek <jasekpetr@gmail.com>
2024-04-29 12:48:29 +0200

    update test requirements

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup, find_packages
 LONG_DESCRIPTION = "Superdesk Server Core"
 
 install_requires = [
-    "urllib3>=1.25.4,<1.27",
+    "urllib3>=1.26.2,<3",
     "eve>=1.1.2,<=2.1.0",
     "eve-elastic>=7.4.0,<7.5.0",
     "elasticsearch<7.14",  # we are using oss version on test server
