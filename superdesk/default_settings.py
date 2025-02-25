967a36c8e62c1628c260f3f7ab91f15a7eef4624
Petr Jasek <jasekpetr@gmail.com>
2025-02-25 09:51:34 +0100

    bump tzlocal version

--- superdesk/default_settings.py
+++ superdesk/default_settings.py
@@ -66,7 +66,7 @@ def celery_queue(name):
 DEFAULT_TIMEZONE = env("DEFAULT_TIMEZONE")
 
 if DEFAULT_TIMEZONE is None:
-    DEFAULT_TIMEZONE = tzlocal.get_localzone().zone
+    DEFAULT_TIMEZONE = tzlocal.get_localzone_name()
 
 if not DEFAULT_TIMEZONE:
     raise ValueError("DEFAULT_TIMEZONE is empty")
