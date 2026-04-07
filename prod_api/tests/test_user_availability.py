50a3582f1ee0978a056912bb9a2db1786e26f66b
Petr Jasek <jasekpetr@gmail.com>
2026-04-07 11:52:30 +0200

    save

--- prod_api/tests/test_user_availability.py
+++ prod_api/tests/test_user_availability.py
@@ -11,10 +11,13 @@ def test_service_get(superdesk_app, prodapi_app_with_data):
     with superdesk_app.app_context():
         user = superdesk_app.data.find_one("users", req=None, username="admin")
         assert user
+        user_no_data = superdesk_app.data.find_one("users", req=None, username="pshenichniy")
+        assert user_no_data
         superdesk_app.data.insert(
             "default_user_availability",
             [
                 {"_id": user["_id"], "enabled": True},
+                {"_id": user_no_data["_id"], "enabled": True},
             ],
         )
     with prodapi_app_with_data.test_client() as client:
@@ -22,11 +25,11 @@ def test_service_get(superdesk_app, prodapi_app_with_data):
         assert resp.status_code == 200
         items = resp.json["_items"]
 
-        assert len(items) == 1
-        assert items[0]["_id"]
-        assert items[0]["username"] == "admin"
+        assert len(items) == 2
 
-        assert len(items[0]["availability"]) == 3
+        admin_item = next(i for i in items if i["username"] == "admin")
+        assert admin_item["_id"]
+        assert len(admin_item["availability"]) == 3
 
         assert {
             "date": "2023-05-15",
@@ -34,7 +37,7 @@ def test_service_get(superdesk_app, prodapi_app_with_data):
             "published_articles": 5,
             "published_events": 0,
             "working_hours": [],
-        } in items[0]["availability"]
+        } in admin_item["availability"]
 
         assert {
             "date": "2023-05-16",
@@ -44,7 +47,7 @@ def test_service_get(superdesk_app, prodapi_app_with_data):
             "working_hours": [
                 {"start": "12:00", "end": "17:00"},
             ],
-        } in items[0]["availability"]
+        } in admin_item["availability"]
 
         assert {
             "date": "2023-05-18",
@@ -52,18 +55,22 @@ def test_service_get(superdesk_app, prodapi_app_with_data):
             "published_articles": 3,
             "published_events": 0,
             "working_hours": [],
-        } in items[0]["availability"]
+        } in admin_item["availability"]
 
-        resp = client.get("/prodapi/v1/user_availability/{}?month=2023-05".format(items[0]["_id"]))
+        no_data_item = next(i for i in items if i["username"] == "pshenichniy")
+        assert no_data_item["_id"]
+        assert no_data_item["availability"] == []
+
+        resp = client.get("/prodapi/v1/user_availability/{}?month=2023-05".format(admin_item["_id"]))
         assert resp.status_code == 200
-        for key in items[0].keys():
+        for key in admin_item.keys():
             if key == "_links":
                 continue
-            assert resp.json[key] == items[0][key]
+            assert resp.json[key] == admin_item[key]
 
         resp = client.get("/prodapi/v1/user_availability")
         assert resp.json["_items"]
-        assert resp.json["_items"][0]["availability"] == []
+        assert all(i["availability"] == [] for i in resp.json["_items"])
 
         resp = client.get("/prodapi/v1/user_availability/{}?month=2023-05".format(bson.ObjectId()))
         assert resp.status_code == 404
